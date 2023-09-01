090d2b14c2432b7ccbf99af2232cd0386191cdb3
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-09-02 06:50:18 +0900

    .

---
+++ tutorial.js
@@ -0,0 +1,28 @@
+var strArr = {
+    span: [
+        {
+            ko: `언어`,
+            en: `Languages`
+        },
+
+        {
+            ko: `기초 문법`,
+            en: `Basic Rules`
+        },
+
+        {
+            ko: `기본 라이브러리`,
+            en: `Basic Libraries`
+        },
+
+        {
+            ko: `기초 문법`,
+            en: `Basic Rules`
+        },
+
+        {
+            ko: `기본 라이브러리`,
+            en: `Basic Libraries`
+        },
+    ]
+}
