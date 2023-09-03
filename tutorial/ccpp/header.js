2244ad8c1fbb6ebd7552a0f316722e02a97bc0b5
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-09-04 06:34:09 +0900

    .

---
+++ tutorial/ccpp/header.js
@@ -0,0 +1,48 @@
+var strArr = {
+    span: [
+        {
+            ko: `언어`,
+            en: `Languages`
+        },
+
+        {
+            ko: `헤더`,
+            en: `Header`
+        },
+
+        {
+            ko: `C에서 기본적으로 지원하는 헤더는 C/C++에서 전부 인식되는 표준 확장자입니다.`,
+            en: ``
+        },
+
+        {
+            ko: `아래 목록에서는 시스템에서 기본적으로 지원하는 헤더 파일을 나열합니다.`,
+            en: ``
+        },
+
+        {
+            ko: `그 외`,
+            en: `Others`
+        },
+
+        {
+            ko: `<a href="#h" class="mark">h</a> 혹은 <a href="#hpp" class="mark">hpp</a> 이외의 확장자명일 경우에도 안에 있는 C/C++로 작성된 코드를 읽을 수 있습니다.`,
+            en: ``
+        },
+
+        {
+            ko: `<a href="../ccpp.html#include" class="includeKey">#include</a>에서 절대 경로 혹은 상대 경로로 작성할 수 있습니다.`,
+            en: ``
+        },
+
+        {
+            ko: `확장자가 없는 <a href="#lib.iostream" class="mark">iostream</a> 등일 경우 실제로 파일을 확장자 없이 작성한 경우입니다.`,
+            en: ``
+        },
+
+        {
+            ko: `아래 목록에서는 시스템에서 기본적으로 지원하는 라이브러리를 나열합니다.`,
+            en: ``
+        }
+    ]
+}
