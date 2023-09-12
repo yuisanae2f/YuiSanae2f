867f3339c36485df1ca28ec1280dd0e8d7c360e3
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-09-12 11:36:45 +0900

    .

--- dictionary/operator.js
+++ dictionary/operator.js
@@ -1,4 +1,7 @@
 strArr = [
     {id: "translate", ko: "언어", en: "Languages"},
-    {id: "operator", ko: "연산자", en: "Operator"}
+    {id: "operator", ko: "연산자", en: "Operator"},
+    {id: "basic", ko: "기본 연산자", en: "Basic Operator"},
+    {id: "caution", ko: "주의", en: "Caution"},
+    {id: "idx", ko: "목록", en: "Summary"}
 ]
---
+++ dictionary/operator/basic.js
@@ -0,0 +1,4 @@
+strArr = [
+    {id: "title", ko: "기본 연산자", en: "Basic Operators"},
+    {id: "translate", ko: "언어", en: "Languages"}
+];
