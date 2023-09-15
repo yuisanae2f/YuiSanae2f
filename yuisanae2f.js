e95fa81dbb0891ce75f82d7e1fe6fcdaea072322
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-09-15 17:56:35 +0900

    .

--- yuisanae2f.js
+++ yuisanae2f.js
@@ -1,3 +1,21 @@
 strArr = [
-    {id: "translate", ko: "언어", en: "Languages"}
+    {id: "translate", ko: "언어", en: "Languages"},
+
+    {
+        id: "cs[0]",
+        ko: '<a href="./tutorial/cs.html">C#</a>으로 작성된 프로젝트입니다. 라이브러리로 사용될 수 있으며, Nuget패키지가 필요한 경우가 많습니다.',
+        en: 'Those projects are written in <a href="./tutorial/cs.html">C#</a> as library.'
+    },
+
+    {
+        id: "StrAICS[0]",
+        ko: "Microsoft.ML의 기능을 사용한 기초적인 기계 학습에 관련된 라이브러리입니다.",
+        en: "Library for the classifier AI 101, written with Microsoft.ML."
+    },
+
+    {
+        id: "CharCraftable[0]",
+        ko: "조합형 문자를 펼치고 펼쳐진 문자열을 다시 조합할 수 있도록 만든 라이브러리입니다.",
+        en: "Library to manage the craftable strings. (e.g. Korean)"
+    }
 ]
