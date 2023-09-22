060ee0537ab23eaabbd3219bccd4d31b12b6847b
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-09-22 13:11:48 +0900

    .

---
+++ dictionary/dataType/integer/charToInt.js
@@ -0,0 +1,19 @@
+function dataTypeCharFromChar() {
+    let _ = document.getElementById("dataType.char.fromChar" + field.c + "._").value;
+    let exe = document.getElementById("dataType.char.fromChar" + field.exe);
+
+    let i = new String(_);
+    exe.innerHTML = 
+    `당신이 입력한 값의 길이는 <a href="#dataType.integer" class="generic">${i.length}</a>입니다. <br><br>`
+    + (
+        i.length == 0 ? `당신은 공백을 입력하셨습니다. 공백문자는 <a class="str", href="#\\0">'\\0'</a>로써, <a href="#dataType.integer" class="generic">0</a>의 값을 지닙니다.` : 
+        i.length == 1 ? 
+        (i.charCodeAt(0) >= 0 && i.charCodeAt(0) <= 127) ? `당신이 입력한 문자를 숫자로 바꾸면 <a href="#dataType.integer" class="generic">${i.charCodeAt(0)}</a>입니다.` :
+        `해당 문자는 ASCII값으로 나타낼 수 없습니다.`
+        : `<a href="#dataType.char" class="reservedKey">char</a>는 문자 하나를 뜻합니다.<br>문자를 하나만 넣고 다시 시도해 봅시다.<br><br>문자 여러 개는 <a href="#array.string" class="str">"문자열"</a>을 참고하세요.`
+    );
+}
+
+function fromChar(_) {
+    return _.charCodeAt(0);
+}
--- dictionary/dataType/struct.js
+++ dictionary/dataType/struct.js
@@ -1,3 +1,5 @@
 const strArr = [
-    {ko: "언어", en: "Languages", id: "translate"}
+    {ko: "언어", en: "Languages", id: "translate"},
+    {id: "summary", ko: "구조체", en: "Structure" },
+    {id: "title", ko: "구조체", en: "Structure" }
 ];
