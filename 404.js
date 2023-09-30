60e605c4d70399036ff6736e595f5b143eedb8d5
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-10-01 04:58:10 +0900

    .

---
+++ 404.js
@@ -0,0 +1,9 @@
+strArr = [
+    {id: "title", en: "Not Found", ko: "찾을 수 없음"},
+    {id: "translate", en: "Languages", ko: "언어"},
+    {
+        id: "missing[0]",
+        en: "The document of this is somehow missing.",
+        ko: "해당 요소에 관한 정보가 아직 작성되지 않았습니다."
+    }
+]
--- yuisanae2f.js
+++ yuisanae2f.js
@@ -2,20 +2,56 @@ strArr = [
     {id: "translate", ko: "언어", en: "Languages"},
 
     {
+        id: "_re[0]",
+        ko: "이것을 보고 있다는 것은 해당 라이브러리에 관한 문서가 준비되지 않았음을 뜻한다.",
+        en: `Seeing this represents that the documentary for this library has not been prepared yet.`
+    },
+
+    {
+        id: "summary[0]",
+        ko: "yuisanae2f의 이름으로 작성된 프로젝트를 나열했습니다.",
+        en: `Just some simple projects`
+    },
+
+    {
+        id: "cpp[0]",
+        ko: `C/C++로 작성된 헤더 모음이다.`,
+        en: `Header bundle made of C/C++.`
+    },
+
+    {
+        id: `cpp[1]`,
+        ko: `윈도우의 Visual Studio에 맞춰져 있다.`,
+        en: `Simple headers available suited for VS in Windows`
+    },
+
+    {
+        id: `ExceptionCPP[0]`,
+        ko: `제 라이브러리에서 사용될 표준 예외 형식.`,
+        en: `The standard exception form in my libraries.`
+    },
+
+    {
         id: "cs[0]",
-        ko: '<a href="./tutorial/cs.html">C#</a>으로 작성된 프로젝트입니다. 라이브러리로 사용될 수 있으며, Nuget패키지가 필요한 경우가 많습니다.',
-        en: 'Those projects are written in <a href="./tutorial/cs.html">C#</a> as library.'
+        ko: 'C#으로 작성된 라이브러리 프로젝트이다.',
+        en: 'Those projects are written in C# as library.'
+    },
+
+    {
+        id: "cs[1]",
+        ko: `<a href="nuget.org"></a>에 업로드되어 있다.`,
+        en: `It is uploaded at <a href="nuget.org"></a>`
     },
 
     {
         id: "StrAICS[0]",
-        ko: "Microsoft.ML의 기능을 사용한 기초적인 기계 학습에 관련된 라이브러리입니다.",
+        ko: "Microsoft.ML의 기능을 사용한 기초적인 기계 학습에 관련된 라이브러리이다.",
         en: "Library for the classifier AI 101, written with Microsoft.ML."
     },
 
     {
         id: "CharCraftable[0]",
-        ko: "조합형 문자를 펼치고 펼쳐진 문자열을 다시 조합할 수 있도록 만든 라이브러리입니다.",
+        ko: "조합형 문자를 펼치고 펼쳐진 문자열을 다시 조합할 수 있도록 만든 라이브러리이다.",
         en: "Library to manage the craftable strings. (e.g. Korean)"
     }
 ]
