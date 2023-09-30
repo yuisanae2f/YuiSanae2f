738bcb7403f0e1bfa3dfa09eabce0146bc7c41d2
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-10-01 06:10:17 +0900

    .

--- dictionary/pointer/array.js
+++ dictionary/pointer/array.js
@@ -1,3 +1,4 @@
 strArr = [
-    {id: "translate", ko: "언어", en: "Languages"}
+    {id: "translate", ko: "언어", en: "Languages"},
+    {id: "string", ko: "문자열", en: "string"}
 ];
--- dictionary/pointer/array/string.js
+++ dictionary/pointer/array/string.js
@@ -1,4 +1,14 @@
 strArr = [
     {id: "translate", ko: "언어", en: "Languages"},
-    {id: "title", ko:"문자열", en: "String"}
+    {id: "title", ko:"문자열", en: "String"},
+    {
+        id: "c_str[1]",
+        ko: "C나 C++에서 사용되는 원시적인 형태의 문자열이다.",
+        en: "is the ancient form which is commonly used in C/C++."
+    },
+    {
+        id: "c_str[2]",
+        ko: `<a href="../../pointer.html">포인터</a>를 사용하여 <a href="../../dataType/integer.html#char">문자</a>의 <a href="../array.html">배열</a>을 표현한다.`,
+        en: `expresses <a href="../array.html">the array</a> of <a href="../../dataType/integer.html#char">the characters</a> using <a href="../../pointer.html">the pointer</a>.`
+    }
 ];
