7acba18d48f3a67acb58a1b6d806d7b06d3ff63f
Yui San ae2f <84163330+yuisanae2f@users.noreply.github.com>
2024-01-02 20:26:42 +0900

    .

--- Component/array.js
+++ Component/array.js
@@ -2,25 +2,33 @@ tent.prototype.array = function(id, lArr, fun = (c) => 'undefined') {
     let table = this.tent(id, 'div');
     table.set('');
     table.pos().style.display = 'flex';
-    // table.pos().style.flexDirection = 'column';
-    table.pos().style.border = '1';
+
+    switch(lArr.length % 2) {
+        case 0: table.pos().style.flexDirection = 'column'; break;
+        case 1: table.pos().style.flexDirection = 'row'; break;
+    }
 
     function go(_id, numArr) {
         let na = numArr.slice();
         const l = na.shift();
         for(let i = 0; i < l; i++) {
-            const d = document.getElementById(_id).tent(`${i}`, 'div');
+            let d = document.getElementById(_id).tent(` ${i}`, 'div');
+
             d.pos().style.display = 'flex';
-            d.pos().style.border = '1px solid';
-            d.pos().style.padding = `${na.length * 5}px`
+            d.pos().style.margin = `${na.length * 5}px`
             switch(na.length % 2) {
                 case 0: d.pos().style.flexDirection = 'column'; break;
                 case 1: d.pos().style.flexDirection = 'row'; break;
             }
 
-            d.set(fun(d.id.split(table.id)[1]));
+            switch(na.length) {
+                case 0:
+                    d.set(fun(d.id.split(table.id)[1]));
+                    d.pos().style.border = '1px solid';
+                default: break;
+            }
 
-            go(d._id, na);
+            go(d.id, na);
         }
     } go(table.id, lArr);
 
